d3728d542add78d0f55dff1b83b14c6b03d594a3
micw1417 <wu355049@gmail.com>
2024-09-19 18:16:58 -0500

    fs

--- src/sheetClient.js
+++ src/sheetClient.js
@@ -1,11 +1,11 @@
-// import { google } from 'googleapis';
+import { google } from 'googleapis';
 // import {client_email, private_key_id,sheet_id} from "../secrets.json"
 
-// export const SHEET_ID = "1WA_EN4Qu6P65vuK8MeNP6sU1o3Cgn5USSjVKt_B_ngs";
-// const client = new google.auth.JWT(client_email, null, private_key_id, [
-//   'https://www.googleapis.com/auth/spreadsheets',
-// ]);
-// const sheets = google.sheets({ version: 'v4', auth: client });
+export const SHEET_ID = "1WA_EN4Qu6P65vuK8MeNP6sU1o3Cgn5USSjVKt_B_ngs";
+const client = new google.auth.JWT("SDS", null, "SDDS", [
+  'https://www.googleapis.com/auth/spreadsheets',
+]);
+const sheets = google.sheets({ version: 'v4', auth: client });
 
-// export default sheets;
+export default sheets;
 
