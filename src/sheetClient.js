4265b63b4f7d1859a1656ae5b0f4053522735aa5
micw1417 <wu355049@gmail.com>
2024-09-19 16:24:01 -0500

    dunno what hapopening i dont understand anymore

--- src/sheetClient.js
+++ src/sheetClient.js
@@ -1,11 +1,8 @@
 import { google } from 'googleapis';
 import {client_email, private_key_id,sheet_id} from "../secrets.json"
 
-const client_email = client_email;
-const private_key = private_key_id;
-export const SHEET_ID = sheet_id;
-
-const client = new google.auth.JWT(client_email, null, private_key, [
+export const SHEET_ID = "1WA_EN4Qu6P65vuK8MeNP6sU1o3Cgn5USSjVKt_B_ngs";
+const client = new google.auth.JWT(client_email, null, private_key_id, [
   'https://www.googleapis.com/auth/spreadsheets',
 ]);
 const sheets = google.sheets({ version: 'v4', auth: client });
